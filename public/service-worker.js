189430c26124c2888999977cf9775bab4c3f66bb
Seth <sethvance26@gmail.com>
2021-07-08 02:15:46 -0400

    fixing css and image icon errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,12 +3,12 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
     './',
-    './assets/css/style.css',
+    './style.css',
     './assets/js/db.js',
     './assets/js/index.js',
     "./manifest.webmanifest",
-    './assets/images/icons/icon-192x192.jpg',
-    './assets/images/icons/icon-512x512.jpg',
+    './assets/icons/icon-192x192.jpg',
+    './assets/icons/icon-512x512.jpg',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
@@ -17,13 +17,10 @@ const FILES_TO_CACHE = [
   // install
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
+      caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
     );
-  
-    self.skipWaiting();
   });
 
 
